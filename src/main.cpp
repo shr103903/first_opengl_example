4442fa416b412767d6aa646da225eabc7bef7726
shr103903 <shr103903@gmail.com>
2024-10-04 11:38:18 +0900

    사각형 그리기

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include "common.h"
 #include "shader.h"
 
+uint32_t CompileShaders();
+void Render();
+void RenderTriangle();
+void RenderRectangle();
+void Startup();
+void Shutdown();
+
+uint32_t renderingProgram;
+uint32_t m_vertexBuffer;
+uint32_t m_vertexArrayObject;
+uint32_t m_indexBuffer;
+
 void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
     SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
     glViewport(0, 0, width, height);
@@ -21,9 +33,136 @@ void OnKeyEvent(GLFWwindow* window,
     }
 }
 
+uint32_t CompileShaders() {
+    uint32_t program = glCreateProgram();
+    if (program == 0){
+        SPDLOG_ERROR("failed to create program");
+        return 0;
+    }
+
+    // 쉐이더 생성
+    auto vertexShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
+    auto fragmentShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
+    SPDLOG_INFO("vertex shader id: {}", vertexShader->Get());
+    SPDLOG_INFO("fragment shader id: {}", fragmentShader->Get());
+    
+    // 쉐이더를 프로그램에 연결
+    glAttachShader(program, vertexShader->Get());
+    glAttachShader(program, fragmentShader->Get());
+    glLinkProgram(program);
+
+    // 프로그램 링크 확인
+    int success = 0;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        char infoLog[1024];
+        glGetProgramInfoLog(program, 1024, nullptr, infoLog);
+        SPDLOG_ERROR("failed to link program");
+        SPDLOG_ERROR("reason: {}", infoLog);
+        return 0;
+    }
+    SPDLOG_INFO("program id: {}", program);
+    return program;
+}
+
 void Render() {
     glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    //glDrawArrays(GL_POINTS, 0, 1);
+    //RenderTriangle();
+    RenderRectangle();
+}
+
+void RenderTriangle() {
+    //glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+void RenderRectangle() {
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+
+void Startup() {
+    auto renderingProgram = CompileShaders();
+    if (renderingProgram == 0) {
+        SPDLOG_ERROR("failed to compile shaders");
+        return;
+    }
+    glUseProgram(renderingProgram);
+    
+    // 점 3개 삼각형
+    // float vertices[] = {
+    //     -0.5f, -0.5f, 0.0f,
+    //     0.5f, -0.5f, 0.0f,
+    //     0.0f, 0.5f, 0.0f,
+    // };
+
+    // 점 6개 삼각형 2개 => 사각형
+    // float vertices[] = {
+    //     // first triangle
+    //     0.5f, 0.5f, 0.0f, // top right
+    //     0.5f, -0.5f, 0.0f, // bottom right
+    //     -0.5f, 0.5f, 0.0f, // top left
+    //     // second triangle
+    //     0.5f, -0.5f, 0.0f, // bottom right
+    //     -0.5f, -0.5f, 0.0f, // bottom left
+    //     -0.5f, 0.5f, 0.0f // top left
+    // };
+
+    // 점 4개 사각형
+    // float vertices[] = {
+    //     0.5f, 0.5f, 0.0f, // top right
+    //     0.5f, -0.5f, 0.0f, // bottom right
+    //     -0.5f, -0.5f, 0.0f, // bottom left
+    //     -0.5f, 0.5f, 0.0f, // top left
+    // };
+    float vertices[] = { 
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top right, red
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right, green
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left, blue
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, // top left, yellow
+    };
+
+    uint32_t indices[] = { // note that we start from 0!
+        0, 1, 3, // first triangle
+        1, 2, 3, // second triange
+    };
+
+    // uint32_t vao = 0;
+    // glGenVertexArrays(1, &vao);
+    // glBindVertexArray(vao);
+
+    // glPointSize(30.0f);
+
+    glGenVertexArrays(1, &m_vertexArrayObject);
+    glBindVertexArray(m_vertexArrayObject);
+
+    glGenBuffers(1, &m_vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,GL_STATIC_DRAW);
+
+    glGenBuffers(1, &m_indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (void*)(sizeof(float) * 3));
+}
+
+void Shutdown() {
+    if(renderingProgram != 0) {
+        glDeleteProgram(renderingProgram);
+    }
+
+    if(m_vertexBuffer != 0) {
+        glDeleteBuffers(1, &m_vertexBuffer);
+    }
+    if(m_vertexArrayObject != 0) {
+        glDeleteVertexArrays(1, &m_vertexArrayObject);
+    }
 }
 
 int main(int argc, const char** argv) {
@@ -67,15 +206,13 @@ int main(int argc, const char** argv) {
     glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
     glfwSetKeyCallback(window, OnKeyEvent);
 
-    auto vertexShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
-    auto fragmentShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
-    SPDLOG_INFO("vertex shader id: {}", vertexShader->Get());
-    SPDLOG_INFO("fragment shader id: {}", fragmentShader->Get());
+    // shader program 생성 및 사용
+    Startup();
 
     // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
     while (!glfwWindowShouldClose(window)) {
-        Render();
+        Render(); // 렌더링 - 점을 그림
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
