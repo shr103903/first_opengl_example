2a24988570eac47f6f717e9ca324f5ef862e67a1
shr103903 <shr103903@gmail.com>
2024-10-18 11:36:25 +0900

    카메라 회전

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,14 @@
 
 uint32_t CompileShaders();
 void Render();
-void RenderTriangle();
-void RenderRectangle();
+// void RenderTriangle();
+// void RenderRectangle();
+// void RenderCube();
 void Startup();
 void Shutdown();
+void ProcessInput(GLFWwindow* window);
+void MouseMove(double x, double y);
+void MouseButton(int button, int action, double x, double y);
 
 uint32_t renderingProgram;
 uint32_t m_vertexBuffer;
@@ -15,6 +19,16 @@ uint32_t m_vertexArrayObject;
 uint32_t m_indexBuffer;
 uint32_t m_texture;
 uint32_t m_texture2;
+
+//camera parameter
+glm::vec3 m_cameraPos { glm::vec3(0.0f, 0.0f, 3.0f) };
+glm::vec3 m_cameraFront { glm::vec3(0.0f, 0.0f, -1.0f) };
+glm::vec3 m_cameraUp { glm::vec3(0.0f, 1.0f, 0.0f) };
+float m_cameraPitch = 0.0f;
+float m_cameraYaw = 0.0f;
+bool m_cameraControl { false };  
+glm::vec2 m_prevMousePos { glm::vec2(0.0f) };
+
 
 void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
     SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
@@ -70,28 +84,64 @@ uint32_t CompileShaders() {
 
 void Render() {
     glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    //glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // transform
+    auto model = glm::rotate(glm::mat4(1.0f),
+      glm::radians((float)glfwGetTime() * 120.0f),
+      glm::vec3(1.0f, 0.5f, 0.0f));
+
+    auto cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    auto cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+    auto cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+    m_cameraFront =  glm::rotate(glm::mat4(1.0f),   
+        glm::radians(m_cameraYaw), glm::vec3(0.0f, 1.0f, 0.0f)) * 
+        glm::rotate(glm::mat4(1.0f), glm::radians(m_cameraPitch), 
+        glm::vec3(1.0f, 0.0f, 0.0f)) * glm::vec4(0.0f, 0.0f, -1.0f, 0.0f);
+
+    // 종횡비 4:3, 세로화각 45도의 원근 투영
+    auto projection = glm::perspective(glm::radians(45.0f),
+    (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.01f, 20.0f);
+    auto view = glm::lookAt(cameraPos, cameraTarget + m_cameraFront, cameraUp);
+
+    auto transform = projection * view * model;
+    auto transformLoc = glGetUniformLocation(renderingProgram, "transform");
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
 
     //glDrawArrays(GL_POINTS, 0, 1);
     //RenderTriangle();
-    RenderRectangle();
+    //RenderRectangle();
+    //RenderCube();
 }
 
-void RenderTriangle() {
-    //glDrawArrays(GL_TRIANGLES, 0, 6);
-}
+// void RenderTriangle() {
+//     //glDrawArrays(GL_TRIANGLES, 0, 6);
+// }
 
-void RenderRectangle() {
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-}
+// void RenderRectangle() {
+//     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+// }
+
+// void RenderCube() {
+//     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  
+//     glEnable(GL_DEPTH_TEST);
+
+//     glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+// }
 
 void Startup() {
-    auto renderingProgram = CompileShaders();
+    renderingProgram = CompileShaders();
     if (renderingProgram == 0) {
         SPDLOG_ERROR("failed to compile shaders");
         return;
     }
     glUseProgram(renderingProgram);
+
+    glEnable(GL_DEPTH_TEST);  
 
     // 이미지 로드
     auto image = Image::Load("./image/wall.jpg");
@@ -99,19 +149,14 @@ void Startup() {
         SPDLOG_ERROR("failed to load image");
         return; // Correct usage of return in a void function
     }
+    SPDLOG_INFO("wall image: {}x{}, {} channels", image->GetWidth(), image->GetHeight(), image->GetChannelCount());
 
     auto image2 = Image::Load("./image/awesomeface.png");
     if (!image2) {
         SPDLOG_ERROR("failed to load awesomeface image");
         return; // Correct usage of return in a void function
     }
-    SPDLOG_INFO("awesomeface image: {}x{}, {} channels",
-    image->GetWidth(), image->GetHeight(), image->GetChannelCount());
-
-    // auto image = Image::Create(512, 512);
-    // image->SetCheckImage(16, 16);
-    
-    SPDLOG_INFO("image: {}x{}, {} channels", image->GetWidth(), image->GetHeight(), image->GetChannelCount());
+    SPDLOG_INFO("awesomeface image: {}x{}, {} channels", image->GetWidth(), image->GetHeight(), image->GetChannelCount());
 
     // 텍스처 생성
     glGenTextures(1, &m_texture); // texture id 생성
@@ -143,16 +188,57 @@ void Startup() {
     glUniform1i(glGetUniformLocation(renderingProgram, "tex2"), 1);
     
     // 정점 데이터
-    float vertices[] = { 
-        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right, red
-        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right, green
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left, blue
-        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, // top left, yellow
+    // float vertices[] = { 
+    //     0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right, red
+    //     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right, green
+    //     -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left, blue
+    //     -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, // top left, yellow
+    // };
+
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f, 0.0f, 1.0f,
+
+        -0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+
+        0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f, 1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f, 0.0f, 0.0f,
+
+        -0.5f,  0.5f, -0.5f, 0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f, 1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f, 1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, 0.0f, 0.0f,
     };
 
-    uint32_t indices[] = { // note that we start from 0!
-        0, 1, 3, // first triangle
-        1, 2, 3, // second triange
+    // uint32_t indices[] = { // note that we start from 0!
+    //     0, 1, 3, // first triangle
+    //     1, 2, 3, // second triange
+    // };
+
+    uint32_t indices[] = {
+        0,  2,  1,  2,  0,  3,
+        4,  5,  6,  6,  7,  4,
+        8,  9, 10, 10, 11,  8,
+        12, 14, 13, 14, 12, 15,
+        16, 17, 18, 18, 19, 16,
+        20, 22, 21, 22, 20, 23,
     };
 
     glGenVertexArrays(1, &m_vertexArrayObject);
@@ -167,15 +253,17 @@ void Startup() {
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, 0);
+    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
 
-    // 꼭지점 색상 지정을 위해 3개의 float를 건너뛰고 3개의 float를 읽는다
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 3));
+    // // 꼭지점 색상 지정을 위해 3개의 float를 건너뛰고 3개의 float를 읽는다
+    // glEnableVertexAttribArray(1);
+    // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 3));
 
     // 텍스처 좌표를 위해 6개의 float를 건너뛰고 2개의 float를 읽는다
     glEnableVertexAttribArray(2);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 6));
+    //glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 6));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (void*)(sizeof(float) * 3));
 
     // for mipmap
     glGenerateMipmap(GL_TEXTURE_2D);
@@ -195,6 +283,78 @@ void Shutdown() {
     if(m_vertexArrayObject != 0) {
         glDeleteVertexArrays(1, &m_vertexArrayObject);
     }
+}
+
+void ProcessInput(GLFWwindow* window) {
+    if (!m_cameraControl)
+        return;
+
+    const float cameraSpeed = 0.0005f; // adjust accordingly
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        m_cameraPos += cameraSpeed * m_cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        m_cameraPos -= cameraSpeed * m_cameraFront;
+
+    auto cameraRight = glm::normalize(glm::cross(m_cameraUp, -m_cameraFront));
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        m_cameraPos += cameraSpeed * cameraRight;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        m_cameraPos -= cameraSpeed * cameraRight;    
+
+    auto cameraUp = glm::normalize(glm::cross(-m_cameraFront, cameraRight));
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        m_cameraPos += cameraSpeed * cameraUp;
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+        m_cameraPos -= cameraSpeed * cameraUp;
+}
+
+void MouseMove(double x, double y) {
+    if (!m_cameraControl)
+        return;
+    auto pos = glm::vec2((float)x, (float)y);
+    auto deltaPos = pos - m_prevMousePos;
+
+    // static glm::vec2 prevPos = glm::vec2((float)x, (float)y);
+    // auto pos = glm::vec2((float)x, (float)y);
+    // auto deltaPos = pos - prevPos;
+
+    const float cameraRotSpeed = 0.8f;
+    m_cameraYaw -= deltaPos.x * cameraRotSpeed;
+    m_cameraPitch -= deltaPos.y * cameraRotSpeed;
+
+    if (m_cameraYaw < 0.0f)   m_cameraYaw += 360.0f;
+    if (m_cameraYaw > 360.0f) m_cameraYaw -= 360.0f;
+
+    if (m_cameraPitch > 89.0f)  m_cameraPitch = 89.0f;
+    if (m_cameraPitch < -89.0f) m_cameraPitch = -89.0f;
+
+    // prevPos = pos;    
+    m_prevMousePos = pos;
+}
+
+void MouseButton(int button, int action, double x, double y) {
+  if (button == GLFW_MOUSE_BUTTON_RIGHT) {
+    if (action == GLFW_PRESS) {
+      // 마우스 조작 시작 시점에 현재 마우스 커서 위치 저장
+      m_prevMousePos = glm::vec2((float)x, (float)y);
+      m_cameraControl = true;
+    }
+    else if (action == GLFW_RELEASE) {
+      m_cameraControl = false;
+    }
+  }
+}
+
+void OnCursorPos(GLFWwindow* window, double x, double y) {
+    glfwGetWindowUserPointer(window);
+    MouseMove(x, y);
+}
+
+void OnMouseButton(GLFWwindow* window, int button, int action, int modifier) {
+  glfwGetWindowUserPointer(window);
+  double x, y;
+  glfwGetCursorPos(window, &x, &y);
+  MouseButton(button, action, x, y);
 }
 
 int main(int argc, const char** argv) {
@@ -237,6 +397,8 @@ int main(int argc, const char** argv) {
     OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
     glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
     glfwSetKeyCallback(window, OnKeyEvent);
+    glfwSetCursorPosCallback(window, OnCursorPos);
+    glfwSetMouseButtonCallback(window, OnMouseButton);
 
     // shader program 생성 및 사용
     Startup();
